fb4512520ddf51642523e1f069b2cc0e085adfb7
Youngmin <lldldudalsll@gmail.com>
2019-04-09 18:17:06 +0900

    add project todo-list

---
+++ todo-list/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoListTemplate form={<Form/>}>
+          template complete
+        </TodoListTemplate>
+      </div>
+    );
+  }
+}
+
+export default App;
