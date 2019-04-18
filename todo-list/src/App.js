8ef3c116fc659e7df0bde23ec66b20cbe6e5c5bc
youngmin <lldldudalsll@gmail.com>
2019-04-18 19:41:18 +0900

    comment arrangement

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -7,10 +7,6 @@ import Palette from './components/Palette'
 const colors = ['#343a40', '#f03e3e', '#12b886', '#228ae6'];
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   console.log(colors)
-  // }
   id = 3 // 이미 0,1,2 가 존재하므로
 
   state = {
