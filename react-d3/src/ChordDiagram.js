95d6daae913992e7f8746036dff01054532f5d3f
youngmin <lldldudalsll@gmail.com>
2019-05-20 18:39:02 +0900

    studying scale func for chord diagram

--- react-d3/src/ChordDiagram.js
+++ react-d3/src/ChordDiagram.js
@@ -27,16 +27,78 @@ class ChordDiagram extends Component {
       [ 1013,   990,  940, 6907]
     ]
 
+    function groupTicks(d, step) {
+      const k = (d.endAngle - d.startAngle) / d.value;
+      return d3.range(0, d.value, step).map(value => {
+        return {value: value, angle: value * k + d.startAngle};
+      });
+    }
+
+    const formatValue = d3.formatPrefix(",.0", 1e3)
+
     const svg = d3.select('body').append('svg')
       .style('width', width).style('height', height).style('margin', margin)
 
     const chord = d3.chord()
+      .padAngle(0.05)
+      .sortSubgroups(d3.descending)
+
+    const arc = d3.arc()
+      .innerRadius(270)
+      .outerRadius(290)
+      
+    const ribbon = d3.ribbon()
+      .radius(270)
+
+    console.log('ribbon', ribbon)
+
+    const color = d3.scaleOrdinal()
+      .domain(d3.range(4))
+      .range(["#000000", "#FFDD89", "#957244", "#F26223"])
+
+    console.log('color', color)
+
+    const asd = d3.scaleLinear().domain([1, 10]).range([0, 100]);
+    console.log('asd', asd(10))
+
     const chords = chord(data);
-    // console.log(chord)
-    const g = svg.append('g').selectAll('g').data(chords.groups).join('g');
+    console.log(chords)
+    const g = svg.append('g').selectAll('g').data(chords.groups).join('g').attr('transform', `translate(${width/2}, ${height/2})`)
 
     g.append('path')
-      .attr('fill', d => d.index)
+      .attr("fill", d => color(d.index))
+      .attr("stroke", d => d3.rgb(color(d.index)).darker())
+      .attr("d", arc);
+
+    const groupTick = g.append("g")
+      .selectAll("g")
+      .data(d => groupTicks(d, 1e3))
+      .join("g")
+      .attr("transform", d => `rotate(${d.angle * 180 / Math.PI - 90}) translate(290,0)`);
+
+    groupTick.append("line")
+      .attr("stroke", "#000")
+      .attr("x2", 6);
+
+    groupTick
+      .filter(d => d.value % 5e3 === 0)
+      .append("text")
+        .attr("x", 8)
+        .attr("dy", ".35em")
+        .attr("transform", d => d.angle > Math.PI ? "rotate(180) translate(-16)" : null)
+        .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
+        .text(d => formatValue(d.value));
+
+    svg.append("g")
+        .attr("fill-opacity", 0.67)
+      .selectAll("path")
+      .data(chords)
+      .join("path")
+        .attr("d", ribbon)
+        .attr("fill", d => color(d.target.index))
+        .attr("stroke", d => d3.rgb(color(d.target.index)).darker())
+        .attr('transform', `translate(${width/2}, ${height/2})`)
+
   }
   render() {
     return (
