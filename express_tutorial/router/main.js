54b3b84d000c21d7cc8546cee8efa0d8c5a7a517
youngmin <lldldudalsll@gmail.com>
2019-05-30 18:40:03 +0900

    trying convert json

--- express_tutorial/router/main.js
+++ express_tutorial/router/main.js
@@ -1,8 +1,67 @@
+const fs = require('fs');
+const lineReader = require('line-reader');
+
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    res.render('index.html')
+
+    let count = 0;
+    const str = [];
+    lineReader.eachLine('./data/gene.txt', function(line, last) {
+      console.log(line)
+      if(count === 0) { // column
+        // str.menu = filterFirstLine(line)
+      } else { // except column
+        str['item' + count] = filterItems(line)
+      }
+
+      count ++; // increase number of line
+
+      if(last) {
+        console.log(' === parsing is done ! === ');
+        console.log(str)
+        makeFile(JSON.stringify(str)); // Save string format, Not object
+      }
+    });
+
+    // function to save file
+    function makeFile() {
+      fs.writeFile("./data/gene.json", str, function(err) {
+        if(err) {
+          return console.log(err)
+        }
+
+        console.log('The file is saved successfully')
+      });
+    }
+
+    // 컬럼 부분 json 으로 컨버팅
+    function filterFirstLine(line) {
+      const arr = line.split('\t');
+      console.log('arr list', arr)
+      return arr;
+    }
+
+    function filterItems(line) {
+      const arr = line.split('\t');
+      const gene = {};
+      if(arr.length === 6) {
+        gene.geneName = arr[0],
+        gene.reference = arr[1],
+        gene.strand = arr[2],
+        gene.start = arr[3],
+        gene.end = arr[4],
+        gene.target = arr[5]
+      }
+      console.log(gene)
+      return gene
+    }
+    
+    res.send(gene)
   });
-  app.get('/about',function(req,res){
+
+
+  app.get('/about',function(req, res) {
     res.render('about.html');
   });
+  
 }
